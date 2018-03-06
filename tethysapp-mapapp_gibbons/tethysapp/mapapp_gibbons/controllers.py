9ad71b60555320b4cb351d1c58f026bca527a1cf
jdgibbons19 <36020894+jdgibbons19@users.noreply.github.com>
2018-03-05 17:35:02 -0700

    Add files via upload

--- tethysapp-mapapp_gibbons/tethysapp/mapapp_gibbons/controllers.py
+++ tethysapp-mapapp_gibbons/tethysapp/mapapp_gibbons/controllers.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from tethys_sdk.gizmos import Button
+from tethys_sdk.gizmos import Button, RangeSlider, TextInput, DatePicker
 
 @login_required()
 def home(request):
@@ -20,12 +20,42 @@ def mapview(request):
     """
     Controller for the app map page.
     """
+    time_slider = RangeSlider(display_text='Time of Day',
+                          name='time_slider',
+                          min=0,
+                          max=24,
+                          initial=12,
+                          step=1
+    )
 
+    adjust_slider = RangeSlider(display_text='Air Quality Adjustment',
+                          name='slider2',
+                          min=-5,
+                          max=5,
+                          initial=0,
+                          step=0.5,
+    )
 
+    search_input = TextInput(display_text='Location Search',
+                            name = 'search_input',
+                            icon_append = 'glyphicon glyphicon-search',
+    )
 
+    date_picker = DatePicker(name='date_picker',
+                             display_text='Date',
+                             autoclose=True,
+                             format='MM d, yyyy',
+                             start_date='03/05/18',
+                             start_view='decade',
+                             today_button=True,
+                             initial='March 5, 2018'
+    )
 
     context = {
-
+        'time_slider': time_slider,
+        'adjust_slider': adjust_slider,
+        'search_input': search_input,
+        'date_picker': date_picker,
     }
 
     return render(request, 'mapapp_gibbons/mapview.html', context)
@@ -48,6 +78,7 @@ def about(request):
     """
     Controller for the app about page.
     """
+
 
 
     context = {
